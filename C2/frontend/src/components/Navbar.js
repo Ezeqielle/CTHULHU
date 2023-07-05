c15b4281f570eed31fd2d0bfde9b03a5f613fe7d
Mathis DI MASCIO <mathis.dimascio@gmail.com>
2023-07-05 15:18:56 +0200

    fix

--- C2/frontend/src/components/Navbar.js
+++ C2/frontend/src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => (
         href="./src"
       >
         <div className="sidebar-brand-icon rotate-n-15">
-          <i class="fa-light fa-computer"></i>
+          <i className="fa-light fa-computer"></i>
         </div>
         <div className="sidebar-brand-text mx-3">
           <span>Admin CTHULHU</span>
